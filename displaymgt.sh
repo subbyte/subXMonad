0b99b27fe7c4ecf4920d14e28eab17ca7ae40856
Xiaokui Shu <subbyte@gmail.com>
2018-05-30 10:03:45 -0400

    fully automate the display management script

--- displaymgt.sh
+++ displaymgt.sh
@@ -28,11 +28,30 @@ DISPEXT=${DISPCONN[1]}
 
 if [ -n "$DISPEXT" ]; then
     if [ $1 = "--mirror" ]; then
-        xrandr --output $DISPLP --mode 1920x1080 --output $DISPEXT --mode 1920x1080 --same-as $DISPLP
+
+        # laptop resolution
+        RESLP=$(xrandr -q | sed -n "/$DISPLP/{n;p;}" | awk '{print $1}')
+
+        # external display resolution
+        RESEXT=$(xrandr -q | sed -n "/$DISPEXT/{n;p;}" | awk '{print $1}')
+
+        # find the smaller resolution and assign it to $RESF
+        RESLPs=(${RESLP//x/ })
+        RESEXTs=(${RESEXT//x/ })
+        if [ "${RESLPs[0]}" -gt "${RESEXTs[0]}" ]; then
+            RESF=$RESEXT
+        else
+            RESF=$RESLP
+        fi
+
+        # mirror the laptop display using the smaller resolution
+        xrandr --output $DISPLP --mode $RESF --output $DISPEXT --mode $RESF --same-as $DISPLP
     else
+        # extended mode
         xrandr --output $DISPLP --auto --primary --output $DISPEXT --auto --right-of $DISPLP
     fi
 else
+    # no external display: remove all display spaces by using "--auto"
     DISPALL=("${DISPCONN[@]}" "${DISPDISCONN[@]}")
     xrandr $(printf " --output %s --auto" ${DISPALL[@]})
 fi
